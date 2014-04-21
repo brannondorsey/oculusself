e5e3d3a92114352aad3e0b623628df4b733bcfb1
Brannon Dorsey <brannon@brannondorsey.com>
2014-04-20 19:08:01 -0500

    First Commit

---
+++ src/testApp.cpp
@@ -0,0 +1,119 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    ofBackground(0);
+    ofHideCursor();
+    mask.loadImage("oculus_mask.png");
+    
+    camWidth = 1138;
+    camHeight = 640;
+	videoGrabber.setDeviceID(0);
+	videoGrabber.setDesiredFrameRate(60);
+	videoGrabber.initGrabber(camWidth, camHeight);
+    videoMirror = new unsigned char[camWidth*camHeight*3];
+    mirrorTexture.allocate(camWidth, camHeight, GL_RGB);
+    webcamY = ofGetHeight()/2 - videoGrabber.getHeight()/2 + 10;
+    xOffset = videoGrabber.getWidth()/2 - 320;
+    cout<<"videoGrabber: "<<videoGrabber.getHeight()/2<<endl;
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+    videoGrabber.update();
+    
+    if (videoGrabber.isFrameNew()) {
+        unsigned char * pixels = videoGrabber.getPixels();
+        for (int i = 0; i < camHeight; i++) {
+            for (int j = 0; j < camWidth*3; j+=3) {
+                // pixel number
+                int pix1 = (i*camWidth*3) + j;
+                int pix2 = (i*camWidth*3) + (j+1);
+                int pix3 = (i*camWidth*3) + (j+2);
+                // mirror pixel number
+                int mir1 = (i*camWidth*3)+1 * (camWidth*3 - j-3);
+                int mir2 = (i*camWidth*3)+1 * (camWidth*3 - j-2);
+                int mir3 = (i*camWidth*3)+1 * (camWidth*3 - j-1);
+                // swap pixels
+                videoMirror[pix1] = pixels[mir1];
+                videoMirror[pix2] = pixels[mir2];
+                videoMirror[pix3] = pixels[mir3];
+            }
+        }
+        mirrorTexture.loadData(videoMirror, camWidth, camHeight, GL_RGB);
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    
+    
+    ofTranslate(0, -30);
+    if (videoGrabber.isInitialized()) {
+        
+        unsigned char * pixels = videoGrabber.getPixels();
+        mirrorTexture.drawSubsection(40,
+                                     webcamY,
+                                     ofGetWidth()/2,
+                                     camHeight,
+                                     xOffset,
+                                     0,
+                                     mirrorTexture.getWidth()/2,
+                                     camHeight);
+
+        mirrorTexture.drawSubsection(ofGetWidth()/2,
+                                     webcamY,
+                                     ofGetWidth()/2,
+                                     camHeight,
+                                     xOffset + 80,
+                                     0,
+                                     mirrorTexture.getWidth()/2,
+                                     camHeight);
+    }
+    mask.draw(0, 0, ofGetWidth(), ofGetHeight());
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
